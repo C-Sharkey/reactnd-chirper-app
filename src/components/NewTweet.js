63a84fb8020ca3799f8a2f51c0ba216d4be8404a
C Sharkey <colinsharkey88@hotmail.com>
2020-05-26 18:49:22 +0100

    New Tweet
    - created 'ADD_TWEET' action
    - added function to add tweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
     state = {
@@ -16,10 +18,9 @@ class NewTweet extends Component {
         e.preventDefault()
 
         const { text } = this.state
+        const { dispatch, id } = this.props
 
-        //todo: add tweet to store
-
-        console.log('New Tweet: ', text)
+        dispatch(handleAddTweet(text, id))
 
         this.setState(() => ({
             text:''
@@ -61,4 +62,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
